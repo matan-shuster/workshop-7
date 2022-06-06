d6d5874df413c31c14e6bb1aaced089dab405410
matan-shuster <82678238+matan-shuster@users.noreply.github.com>
2022-06-06 19:00:43 +0300

    D

--- JediService.js
+++ JediService.js
@@ -3,14 +3,30 @@ const jediFile = 'jedi_list.json';
 
 async function replaceJedi(jediId, jedi) {
     //TODO write logic replacing jedi by it's id with newly passed jedi
+    const jediList = await getAll();
+    jediList[jediId] = jedi;
+    await writeJediFile(jediList);
 }
 
 async function deleteJedi(id) {
     //TODO Delete jedi by given id in our file
+    let data = await readJediFile()
+    const index = data.findIndex(jedi => jedi.id === id)
+    if(index === -1) {
+        return false;
+    }
+    else{
+        data.splice(index, 1)
+        await writeJediFile(data);
+        return ("Jedi deleted");
+    }
+
 }
 
 async function getAll() {
     //TODO obtain all saved jedis and return it to callee
+    let data = await readJediFile();
+    return data;
 }
 
 async function addJedi(jedi) {
@@ -30,7 +46,6 @@ async function getJedi(id) {
 async function readJediFile() {
     try {
         const data = await fs.readFile(jediFile);
-        console.log(data.toString());
         return JSON.parse(data.toString());
     } catch (error) {
         console.error(`Got an error trying to read the file: ${error.message}`);
@@ -45,10 +60,19 @@ async function writeJediFile(content) {
     }
 }
 
+async function darkSide(){
+    const data = await readJediFile();
+    const darkSide = data.indexOf(jedi => jedi.name.includes("Anakin Skywalker"));
+    console.log(data[darkSide]);
+    data[darkSide].name = "Darth Vader";
+    await writeJediFile(data);
+}
+
 module.exports = {
     addJedi,
     getJedi,
     getAll,
     replaceJedi,
     deleteJedi,
+    darkSide
 };
--- app.js
+++ app.js
@@ -34,11 +34,55 @@ app.get("/jedi/:id", async (req, res) => {
 });
 
 //TODO 1. create GET /jedi route and handle logic inside of it
+app.get("/jedi", async (req, res) => {
+    const jedi = await jediService.getAll();
+    res.status(200).json(jedi);
+});
 
 //TODO 2. create PUT /jedi/:id route and handle logic of updating jedi that already exists in the list.
-//Dont forget to take care of errors (e.g. jedi with id not exists)
 
+app.put("/jedi/:id", async (req, res) => {
+    let jediId = Number.parseInt(req.params.id);
+
+    if (isNaN(jediId)) return res.status(400).json({
+        status: 400,
+        error: "wrong parameters"
+    });
+
+    const jedi = await jediService.getJedi(jediId);
+
+    if (!jedi) return res.status(404).json({
+        status: 404,
+        error: "Not found"
+    });
+
+    await jediService.replaceJedi(jediId, req.body);
+    res.status(200).json(req.body);
+});
 //TODO 3. create DELETE /jedi/:id route and handle logic of deleting jedi
+app.delete("/jedi/:id", async (req, res) => {
+
+
+    let jediId = Number.parseInt(req.params.id);
+
+    if (isNaN(jediId)) return res.status(400).json({
+        status: 400,
+        error: "wrong parameters"
+    });
+
+    const jedi = await jediService.deleteJedi(jediId);
+    if(!jedi) return res.status(404).json({
+        status: 404,
+        error: "Not found"
+    });
+
+    res.status(200).json(await jediService.getAll());
+});
+
+app.put("/jedi/dark_side", async (req, res) => {
+    const jedi = await jediService.darkSide();
+    res.status(200).json(jedi);
+});
 
 app.listen(port, () => {
     console.log("Server started on port", port);
